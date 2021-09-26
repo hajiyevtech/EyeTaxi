257ec967434598efc99840232eb0b1b44411c7b2
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-09-26 19:51:53 +0400

    Some packages removed from solution.

--- EyeTaxi/Views/MainView.xaml.cs
+++ EyeTaxi/Views/MainView.xaml.cs
@@ -11,8 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using GMap.NET;
-using GMap.NET.MapProviders;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
 using EyeTaxi.ViewModels;
