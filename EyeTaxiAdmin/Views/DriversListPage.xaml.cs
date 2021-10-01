1d01176e4929cf883a1088435df5efcfb0e7751b
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-10-01 17:57:45 +0400

    er

--- EyeTaxiAdmin/Views/DriversListPage.xaml.cs
+++ EyeTaxiAdmin/Views/DriversListPage.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
+using System.Windows.Input; 
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -23,6 +23,7 @@ namespace EyeTaxiAdmin.Views
     public partial class DriversListPage : Page
     {
         //public DriverListPageModel VM { get; set; }
+        
         public DriversListPage()
         {
             InitializeComponent();
