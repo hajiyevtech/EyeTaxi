efb77bab66e1838ed0fdf933a5d3f641eb64d09f
nicat ceferli <mega.ceferli@gmail.com>
2021-10-01 21:33:08 +0400

    Hey

---
+++ EyeTaxiAdmin/ViewModel/AdminLoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using EyeTaxi.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EyeTaxiAdmin.ViewModel
+{
+    public class AdminLoginViewModel
+    {
+        public RelayCommand CloseButtonClickCommand { get; set; }
+        public AdminLoginViewModel()
+        {
+
+            CloseButtonClickCommand = new RelayCommand(s =>
+            {
+                if (s is Window window)
+                {
+                    window.Close();
+                }
+            });
+        }
+    }
+}
